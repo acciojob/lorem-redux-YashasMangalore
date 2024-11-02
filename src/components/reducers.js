4701149324a14d9fcccbdfd136fb8a36e009e011
Yashas <yashasjob@gmail.com>
2024-11-02 20:56:37 +0530

    commit

---
+++ src/components/reducers.js
@@ -0,0 +1,13 @@
+import InitialState from "./InitialState";
+
+const Reducers = (state = InitialState, action) => {
+  switch (action.type) {
+    case "UPDATE":
+      return action.post;
+
+    default:
+      return state;
+  }
+};
+
+export default Reducers;
